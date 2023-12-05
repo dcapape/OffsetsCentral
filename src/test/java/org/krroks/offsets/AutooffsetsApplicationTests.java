b6089c03512759dad3de0dea8311f46b2aeac710
Diego Capapé <diego.capape@gmail.com>
2023-12-05 01:34:13 +0100

    Initial Release

---
+++ src/test/java/org/krroks/offsets/AutooffsetsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.krroks.offsets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutooffsetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
